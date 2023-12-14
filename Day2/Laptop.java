003df23dbb25a1ab5f43a9efca4a3f29c2de8df6
pratheep0401 <153702833+pratheep0401@users.noreply.github.com>
2023-12-14 06:48:11 +0530

    Add files via upload
    Day2 Assignment

---
+++ Day2/Laptop.java
@@ -0,0 +1,22 @@
+package week1.Day2;
+
+public class Laptop {
+public static void main(String[] args) {
+	byte weight      =2;
+	short screenSize =12;
+	int quantity     =12345;
+	long imei        =97654323445456l;
+	float breath     =9.23f;
+	double thickness =2.3566811145456745d;
+	char firstletter ='L';
+	boolean damage   =true;
+	System.out.println("WEIGHT OF LAPTOP IN KG's           = "+weight);
+	System.out.println("SCREEN SIZE IN INCHES              = "+screenSize);
+	System.out.println("QUANTITY OF LAPTOP                 = "+quantity);
+	System.out.println("IMEI NO                            : "+imei);
+	System.out.println("BREATH OF LAPTOP                   = "+breath);
+	System.out.println("THICKNESS                          = "+thickness);
+	System.out.println("FIRST LETTER OF THE BRAND NAME IS  : "+firstletter);
+	System.out.println("IS LAPTOP DAMAGED?                 : "+damage);
+}
+}
---
+++ Day2/StudentDetails.java
@@ -0,0 +1,25 @@
+package week1.Day2;
+
+public class StudentDetails {
+	public static void main(String[] args) {
+		
+
+String studentname,fathername,mothername,address;
+studentname  ="PREM";
+fathername   = "JOHN";
+mothername   = "MARY"; 
+address      = "pazhavanthangal,chennai";
+byte weight  = 40;
+long emis_no = 8994578498l;
+short rollno = 633;
+float height = 123.5f;
+System.out.println("NAME OF THE STUDENT IS : "+studentname);
+System.out.println("STUDENT'S FATHER NAME  : "+fathername);
+System.out.println("STUDENT'S MOTHER NAME  : "+mothername);
+System.out.println("STUDENT'S ADDRESS      : "+address);
+System.out.println("STUDENT'S ROLL NO      : "+rollno);
+System.out.println("STUDENT'S EMIS NO      : "+emis_no);
+System.out.println("STUDENT'S WEIGHT       : "+weight);
+System.out.println("STUDENT'S HEIGHT       : "+height);
+}
+}
